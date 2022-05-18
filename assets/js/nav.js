60acb4524e789f4bba800a0c2293c4c22ebab133
Asad Khilji <khiljiasad2@gmail.com>
2022-05-17 21:39:39 -0400

    first commit

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -6,7 +6,7 @@ const createNav = () => {
         <a href="Quran.html">Quran</a>
         <a href="Hadiths.html">Hadith</a>
         <a href=#">Seerah</a>
-        <a href="#">Arabic</a>
+        <a href="Arabic.html">Arabic</a>
         <a href="Books.html">Islamic Books</a>
     </nav>
     `;
